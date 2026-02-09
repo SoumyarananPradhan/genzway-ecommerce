421527377b488069464685138ffc09c1c2e6a02d
Soumyaranjan <soumyaranjanaman54@gmail.com>
2026-02-09 16:19:34 +0530

    Update website favicon

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -139,8 +139,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.User'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
